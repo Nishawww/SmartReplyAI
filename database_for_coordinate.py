001de66eb04042125960a93a79734e1b20ef5908
Nishawww <nisha846437@gmail.com>
2025-02-18 16:43:39 +0530

    Update database_for_coordinate.py

--- database_for_coordinate.py
+++ database_for_coordinate.py
@@ -5,18 +5,16 @@ import json
 import os
 from dotenv import load_dotenv
 from swarm import Swarm, Agent
-
+load_dotenv()
 # Initialize OpenAI and Pinecone
-openai.api_key = "sk-svcacct-f-w8Kl1PbYP-xfJ0mJA5tJ9iMGLfZNBlJWfPNrpzLak9H-MO2orHBdyIa7a5tWT3BlbkFJGUdItJUijerYpfDpR29p_RjNJpfTflOgFdugRlWXm21PB3Rfn5kqZ4FkmmzD8A"
-api_key = "pcsk_6unayn_7NDiyFLZmevgcuGCz1cp5xm39AHDrm7zoWNREmtVJHPr4Nb9CBzczeXQBpqjohG"
-pc = Pinecone(api_key=api_key)   
+pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])   
 
 # Connect to existing index
 index_name = "email-rag"
 index = pc.Index(index_name)
 
 # Initialize OpenAI client and Swarm
-openai_client = OpenAI(api_key=openai.api_key)
+openai_client = OpenAI(api_key=os.environ["OPEN_API_KEY"])
 swarm = Swarm(client=openai_client)
 
 def retrieve_for_policies(query: str):
